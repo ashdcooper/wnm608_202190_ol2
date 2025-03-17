18921a6154bde578680a9444a61e78beee109df0
Ashley Student <ashleystudent@Mac.attlocal.net>
2025-03-17 10:55:17 -0500

    Meta.php Update Test

--- cooper.ashley/parts/meta.php
+++ cooper.ashley/parts/meta.php
@@ -3,6 +3,6 @@
 
 <base href="https://ashleycooper-2024.com/aau/wnm608_202190_ol2/cooper.ashley/">
 
-<link rel="stylesheet" href="../lib/css/styleguide.css">
-<link rel="stylesheet" href="../lib/css/gridsystem.css">
-<link rel="stylesheet" href="../css/storetheme.css">
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">
